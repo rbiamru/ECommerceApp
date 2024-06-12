d083372a0daa55e1dffbee4411c1253856fb46e7
Amruthan Manjula Anandan <rbiamru@gmail.com>
2024-06-12 10:50:37 -0400

    Commit overall structure of the working test code with project referenced

---
+++ ECommerceApp1.nUnitTests/ProductTests.cs
@@ -0,0 +1,22 @@
+namespace ECommerceApp1.nUnitTests
+{
+    public class ProductTests
+    {
+        
+        [SetUp]
+        public void Setup()
+        {
+       
+        }
+
+        // Tests for ProductID
+       [Test]
+        public void ProductId_Test()
+        {
+            Assert.Pass();
+
+        }
+
+       
+    }
+}
